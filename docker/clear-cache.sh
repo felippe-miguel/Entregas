e1cc7fbf5c9b4a0ef6d08c820af53e7f7d446543
Felippe Miguel <felippe.miguel.silva@gmail.com>
2020-07-13 15:30:40 -0300

    Configurando permissões para rodar em linux

--- docker/clear-cache.sh
+++ docker/clear-cache.sh
@@ -3,7 +3,7 @@ echo "==> Generating key"
 php artisan key:generate
 
 echo "==> Clear and config cache"
+php artisan view:clear
 php artisan cache:clear
 php artisan route:clear
 php artisan config:cache
-php artisan view:clear
