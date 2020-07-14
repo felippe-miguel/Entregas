7204a76f717a6008197a2f6bc029d97982beaa88
Felippe Miguel <felippe.miguel.silva@gmail.com>
2020-07-13 23:20:51 -0300

    Adicionado trecho para exibição de mensagens de feedback

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -5,6 +5,15 @@
   </head>
   <body>
     @include('partials.navbar')
+
+    <div class="container">
+      @if(Session::has('message'))
+        <div class="alert alert-{{Session::get('status')}} mt-5" role="alert">
+          {{ Session::get('message') }}
+        </div>
+      @endif
+    </div>
+
     @yield('content')
   </body>
 </html>
