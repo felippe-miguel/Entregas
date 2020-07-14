f5e3b5e136347e27e24b218400c2b3671344d02d
Felippe Miguel <felippe.miguel.silva@gmail.com>
2020-07-13 23:17:55 -0300

    Script de importação dos clientes com parse dos dados

---
+++ app/app/Imports/CustomersImport.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Imports;
+
+use App\Customer;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithStartRow;
+
+class CustomersImport implements ToModel, WithStartRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        $splitted_address = $this->splitAddress($row[4]);
+        
+        return new Customer([
+          'name'          => $row[0],
+          'email'         => $row[1],
+          'birth_date'    => $row[2],
+          'cpf'           => $row[3],
+          'address'       => $splitted_address['street'],
+          'number'        => $splitted_address['number'],
+          'complement'    => $splitted_address['complement'],
+          'neighborhood'  => $splitted_address['neighborhood'],
+          'city'          => $splitted_address['city'],
+          'cep'           => $row[5]
+        ]);
+    }
+
+    private function splitAddress($complete_address)
+    {
+        $exploded_address = explode( " - ", $complete_address );
+        $street = explode(",", $exploded_address[0])[0];
+        $neighborhood = $exploded_address[1];
+        $city = $exploded_address[2];
+        $number_and_complement = explode(" ", explode(",", $exploded_address[0])[1]);
+        $number = $number_and_complement[1];
+        $complement = implode(" ", array_slice($number_and_complement, 2));
+        $complement = ($complement != '') ? $complement : null;
+
+        $splitted_address = [
+            'street' => $street,
+            'number' => $number,
+            'complement' => $complement,
+            'neighborhood'  => $neighborhood,
+            'city' => $city
+        ];
+
+        return $splitted_address;
+    }
+
+    /**
+     * @return int
+     */
+    public function startRow(): int
+    {
+        return 2;
+    }
+}
