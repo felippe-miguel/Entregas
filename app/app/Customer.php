1ae3238c07b571b5922acf862c3a2e4ce90708d4
Felippe Miguel <felippe.miguel.silva@gmail.com>
2020-07-13 23:18:29 -0300

    Parse da data de aniversário do cliente, para armazenamento correto no banco de dados

--- app/app/Customer.php
+++ app/app/Customer.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Customer extends Model
 {
+    protected $dates = [
+        'birth_date',
+    ];
+
     protected $fillable = [
         'name',
         'email',
@@ -18,4 +23,15 @@ class Customer extends Model
         'city',
         'cep'
     ];
+
+    public function getBirthDateAttribute($birth_date)
+    {
+        return Carbon::parse($birth_date)->format('d/m/Y');
+    }
+
+    public function setBirthDateAttribute($birth_date)
+    {
+        $birth_date = Carbon::createFromFormat('d/m/Y', $birth_date);
+        $this->attributes['birth_date'] = Carbon::parse($birth_date)->format('Y-m-d');
+    }
 }
