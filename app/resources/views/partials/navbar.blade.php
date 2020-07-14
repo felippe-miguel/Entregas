e39c0d8c914029f90a91d1d4dba0a61158330941
Felippe Miguel <felippe.miguel.silva@gmail.com>
2020-07-13 23:21:15 -0300

    Navbar padronizado conforme exemplos do bootstrap

--- app/resources/views/partials/navbar.blade.php
+++ app/resources/views/partials/navbar.blade.php
@@ -1,7 +1,14 @@
-<div class="bg-dark">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <nav class="py-2">
-      <a class="text-white" href="/">Home</a>
-    </nav>
+    <a class="navbar-brand" href="/">Entregas</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-item nav-link" href="{{ route('home') }}">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="{{ route('customers.index') }}">Clientes</a>
+      </div>
+    </div>
   </div>
-</div>
+</nav>
